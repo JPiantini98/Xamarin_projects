b690f23977b256fdea8177259df88cb10e0a319b
JPiantini98 <45038839+JPiantini98@users.noreply.github.com>
2020-11-12 12:00:40 -0400

    Project 1 Creation
    Asignment: Create a Xamarin forms application and change at least 3 UI properties.

---
+++ Project_Intro/Project_Intro/Project_Intro/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Project_Intro
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
